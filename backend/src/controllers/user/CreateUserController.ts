c6682191083e33364968a092a2c8a3ea7a228968
Lucas Davila <nightdavila@gmail.com>
2023-09-26 09:56:48 -0300

    Feita toda a configuração do prisma e dos middlewares de erro.

---
+++ backend/src/controllers/user/CreateUserController.ts
@@ -0,0 +1,11 @@
+import {Request, Response} from 'express'
+
+class CreateUserController {
+  async handle(req: Request, res: Response) {
+    
+
+    return res.json({ok: true})
+  }
+}
+
+export { CreateUserController }
--- backend/src/middlewares/errors.ts
+++ backend/src/middlewares/errors.ts
@@ -1,16 +1,11 @@
 import express, {Request, Response, NextFunction}  from "express";
 import 'express-async-errors'
-import cors from 'cors';
 
-import { router } from "./routes";
+// TODO: Transferir para a logica de controle de erros no arquivos server.ts
 
-const app = express();
+const errors = express();
 
-app.use(express.json());
-app.use(cors());
-app.use(router);
-
-app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+errors.use((err: Error, req: Request, res: Response, next: NextFunction) => {
   if(err instanceof Error) {
     return res.status(400).json({
       error: err.message
@@ -24,6 +19,4 @@ app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
 
 })
 
-
-
-app.listen(3333, () => console.log('Servidor Online!!'))
+export {errors}
