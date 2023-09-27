fd43e3c6e7ff8ccdeb370446bad7d97989cea3be
Lucas Davila <nightdavila@gmail.com>
2023-09-27 13:20:04 -0300

    Feita a logica no backend para cadastro de usuario ja com a senha sendo salva criptografada via bcryptjs.

--- backend/src/controllers/user/CreateUserController.ts
+++ backend/src/controllers/user/CreateUserController.ts
@@ -1,10 +1,19 @@
 import {Request, Response} from 'express'
+import { CreateUserService } from '../../services/user/CreateUserService'
 
 class CreateUserController {
   async handle(req: Request, res: Response) {
-    
+    const { name, email, password } = req.body;
 
-    return res.json({ok: true})
+    const createUserService = new CreateUserService();
+
+    const user = await createUserService.execute({
+      name, 
+      email, 
+      password
+    });
+    
+    return res.json(user)
   }
 }
 
