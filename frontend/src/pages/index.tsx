2e2650c85e2327e68187032075e32fe6977e6ca9
Lucas Davila <nightdavila@gmail.com>
2023-10-01 19:14:02 -0300

    Estrutura basica do front end criado

---
+++ frontend/src/pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from "next/head"
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Lucas Restaurant - Faça seu login</title>
+      </Head>
+      <div>
+        <h1>Lucas Pizzaria</h1>
+      </div>
+    </>
+  )
+}
