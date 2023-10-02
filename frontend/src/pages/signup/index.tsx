f320b8c1e33f7a457b9f081a615f2a5c75e670f2
Lucas Davila <nightdavila@gmail.com>
2023-10-02 00:22:30 -0300

    Criada a pagina de login seguindo a estilização da aplicação.

--- frontend/src/pages/signup/index.tsx
+++ frontend/src/pages/signup/index.tsx
@@ -1,33 +1,35 @@
 import Head from "next/head"
 import Image from "next/image"
-import styles from '../../styles/home.module.scss'
-import logoImg from '../../public/logo.svg'
+import styles from '../../../styles/home.module.scss'
+import logoImg from '../../../public/logo.svg'
 
-import { Input } from "../components/ui/Input/index"
-import { Button } from "../components/ui/Button"
+import { Input } from "../../components/ui/Input/index"
+import { Button } from "../../components/ui/Button"
 
 import Link from "next/link"
 
-export default function Home() {
+export default function Signup() {
   return (
     <>
       <Head>
-        <title>Lucas Restaurant - Faça seu login</title>
+        <title>Faça seu cadastro agora!</title>
       </Head>
       <div className={styles.containerCenter}>
         <Image src={logoImg} alt="Logo Lucas Pizzaria" />
         <div className={styles.login}>
+          <h1>Criando sua conta</h1>
           <form>
+          <Input placeholder="Digite seu nome" type="text" />
             <Input placeholder="Digite seu email" type="email" />
             <Input placeholder="Digite sua senha" type="password" />
             <Button
               type="submit"
               loading={false}
             >
-              Acessar
+              cadastrar
             </Button>
           </form>
-          <Link legacyBehavior href="/signup"><a className={styles.text}> Não possui uma conta? Cadastre-se</a></Link>
+          <Link legacyBehavior href="/#"><a className={styles.text}>Já possui uma conta? Faça o login</a></Link>
         </div>
       </div>
     </>
