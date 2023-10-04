da3d236b90ae55faa454a685b6288a5ae46f8b5d
Lucas Davila <nightdavila@gmail.com>
2023-10-04 11:47:57 -0300

    Criada a logina de login e context api para o mesmo.

---
+++ frontend/src/contexts/AuthContext.tsx
@@ -0,0 +1,108 @@
+import { createContext, ReactNode, useState } from "react";
+
+import { api } from "../services/apiClient";
+
+import { destroyCookie, setCookie, parseCookies } from "nookies";
+import Router from "next/router";
+
+type AuthContextData = {
+  user: UserProps;
+  isAuthenticated: boolean;
+  signIn: (credentials: SignInProps) => Promise<void>;
+  signOut: () => void;
+  signUp: (credentials: SignUpProps) => void;
+}
+
+type UserProps = {
+  id: string;
+  name: string;
+  email: string;
+}
+
+type SignInProps = {
+  email: string;
+  password: string;
+}
+
+type SignUpProps = {
+  name: string;
+  email: string;
+  password: string;
+}
+
+type AuthProviderProps = {
+  children: ReactNode;
+}
+
+export const AuthContext = createContext({} as AuthContextData)
+
+export function signOut(){
+  try{
+    destroyCookie(undefined, '@nextauth.token')
+    Router.push('/')
+  }catch{
+    console.log('erro ao deslogar')
+  }
+}
+
+export function AuthProvider({ children }: AuthProviderProps) {
+  const [user, setUser] = useState<UserProps>()
+  const isAuthenticated = !!user;
+
+  async function signIn({ email, password}: SignInProps){
+    try {
+      const response = await api.post('/session', {
+        email,
+        password
+      })
+
+      const { id, name, token } = response.data;
+
+      setCookie(undefined, '@nextauth.token', response.data.token, {
+        maxAge: 60 * 60 * 24 * 30, // Exporar em 1 mes
+        path: "/" , // Quais caminhos acessam o cookie
+      })
+
+      // Passar para proximas requisições o nosso token
+
+      setUser({
+        id,
+        name,
+        email,
+      })
+
+      // Passar para proximas reqisições o token
+      api.defaults.headers['Autorization'] = `Bearer ${token}`
+
+      // Redireciona o user para a /dashboard
+      Router.push('/dashboard')
+
+    }   catch (err) {
+      console.log('ERRO AO ACESSAR', err)
+    }
+  }
+
+  async function signUp({ name, email, password }: SignUpProps) {
+    try {
+      
+      const response = await api.post('/users', {
+        name,
+        email,
+        password
+      })
+
+      console.log("Cadastrado com sucesso!")
+
+      Router.push('/')
+
+    } catch (error) {
+      console.log("Erro ao cadastrar ", error)
+    }
+  }
+
+  return (
+    <AuthContext.Provider value={{ user, isAuthenticated, signIn, signOut, signUp }}>
+      {children}
+    </AuthContext.Provider>
+  )
+}
--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,3 +1,5 @@
+import { FormEvent, useContext, useState } from "react"
+
 import Head from "next/head"
 import Image from "next/image"
 import styles from '../../styles/home.module.scss'
@@ -6,9 +8,38 @@ import logoImg from '../../public/logo.svg'
 import { Input } from "../components/ui/Input/index"
 import { Button } from "../components/ui/Button"
 
+import { AuthContext } from '../contexts/AuthContext'
+
 import Link from "next/link"
 
 export default function Home() {
+  const { signIn } = useContext(AuthContext)
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [loading, setLoading] = useState(false);
+
+  async function handleLogin(event: FormEvent) {
+    event.preventDefault();
+
+    // Evitar que o user envie um dado vazio no email e senha
+    if(email === '' || password === ''){
+      alert('PREENCHA OS DADOS')
+      return;
+    }
+
+    setLoading(true);
+
+    let data = {
+      email,
+      password
+    }
+
+    await signIn(data);
+
+    setLoading(false)
+  }
+
   return (
     <>
       <Head>
@@ -17,12 +48,22 @@ export default function Home() {
       <div className={styles.containerCenter}>
         <Image src={logoImg} alt="Logo Lucas Pizzaria" />
         <div className={styles.login}>
-          <form>
-            <Input placeholder="Digite seu email" type="email" />
-            <Input placeholder="Digite sua senha" type="password" />
+          <form onSubmit={handleLogin}>
+            <Input
+              placeholder="Digite seu email"
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <Input
+              placeholder="Digite sua senha"
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
             <Button
               type="submit"
-              loading={false}
+              loading={loading}
             >
               Acessar
             </Button>
